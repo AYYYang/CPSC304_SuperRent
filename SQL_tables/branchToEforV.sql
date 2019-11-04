2c52adb3d2ff177eff6beafd45b00bca695e5c4c
AmandaYang <yuanyuan.yang@alumni.ubc.ca>
2019-11-04 15:27:45 -0800

    Inserted some tuples for branchToEforV tables; made changes to etname and vtname from int to varchar

--- SQL_tables/branchToEforV.sql
+++ SQL_tables/branchToEforV.sql
@@ -23,7 +23,7 @@ create table Branch(
 -- primary key vtname
 create table VehicleType(
 	vtname varchar(50) primary key,
-	features varchar(50),
+	features varchar(300),
 	wrate decimal check (wrate between 0 and 10^38 -1),
 	drate decimal check (drate between 0 and 10^38 -1),
 	hrate decimal check (hrate between 0 and 10^38 -1),
@@ -37,7 +37,7 @@ create table VehicleType(
 -- foreignkey vtname refers to VehicleType
 -- foreignkey location, city refers to Branch
 create table Vehicle(
-	vid int check (vid between 0 and 9999999999) primary key,
+	vid varchar(10) check (vid between 0 and 9999999999) primary key,
 	vlicense varchar(20),
 	make varchar(20),
 	model varchar(20),
@@ -59,14 +59,14 @@ create table EquipType(
 	etname varchar(20) primary key,
 	drate decimal check (drate between 0 and 10^38 -1), 
 	hrate decimal check (krate between 0 and 10^38 -1),
-);
+	);
 
 -- Equipment (eid, etname, status, location, city) 
 -- primary key eid
 -- foreign key etname, location, city
 -- constraints: status can take the values ( available, rented, not_available)
 create table Equipment (
-	eid int check (eid between 0 and 9999999999) primary key, 
+	eid varchar(10) check (eid between 0 and 9999999999) primary key, 
 	etname varchar(20),
 	foreign key(etname) references EquipType(etname), 
 	status varchar check (status = "available" OR status = "not_available" OR status = "rented"), 
@@ -83,6 +83,61 @@ create table EforV(
 	foreign key(etname) references EquipType, 
 	foreign key(vtname) references VehicleType,
 );
+
+
+-- Insertion Branch
+insert into Branch values("Vancouver International Airport", "Vancouver");
+insert into Branch values("UBC", "Vancouver");	
+insert into Branch values("Parliament", "Ottawa");
+
+-- Insertion VehicleType(vtname, features, wrate, drate, hrate, wirate, dirate, hirate, krate)) 
+insert into VehicleType("Economy", "Automatic Transmission, A/C, 4 Doors", 280.78, 32.90, 5.87, 20, 9, 0.99, 2.6)
+insert into VehicleType("Compact", "Stick, 2 Doors", 290.78, 33.90, 9.87, 9, 8, 1.23, 3.7)
+insert into VehicleType("Mid-size", "Automatic Transmission, 4 Doors ", 300.78, 41.90, 10.87, 9, 7.99, 4.23, 3.8)
+insert into VehicleType("Standard", "Automatic Transmission", 500.78, 300.90, 90.87, , 7.99, 4.23, 9.8)
+insert into VehicleType("Fullsize", "Automatic Transmission, 4 Doors, A/C, Child-safe", 450.78, 270.90, 90.87, 9, 7.99, 4.23, 9.8)
+insert into VehicleType("SUV", "Automatic Transmission, 4 Doors, A/C, Leather Seats", 1000.78, 300.90, 90.87, 9, 7.99, 4.23, 100)
+insert into VehicleType("Truck", "Stick, 2 Doors, 1t Storage, Winter Tires", 1000.78, 300.90, 90.87, 9, 7.99, 4.23, 100)
+
+-- Insertion Vehicle (vid, vlicense, make, model, year, color, odometer, status, vtname, location, city) 	
+insert into Vehicle(0000000001, 25441163, "BMW", "X5", 2017, "salmon", 9800, "for_rent", "SUV", "UBC", "Vancouver")
+insert into Vehicle(9873124183, 25441162, "Hyundai", "Y998", 2009, "gray", 19823, "for_rent", "Economy", "Parliament", "Ottawa")
+insert into Vehicle(0000000002, 25441161, "BMW", "X5", 2017, "salmon", 9800, "for_sale", "SUV", "UBC", "Vancouver")
+
+-- Insert EquipType(etname, drate, hrate) 
+insert into EquipType("Ski Rack", 23, 5.99)
+insert into EquipType("Child Safety Seats", 39, 8.99)
+insert into EquipType("Lift gate and Car-towing equipment", 68.99, 10.99)
+
+-- Insertion Equipment (eid, etname, status, location, city) 
+insert into Equipment(9999999999, "Ski Rack", "available", "Vancouver International Airport", "Vancouver")
+insert into Equipment(9999999998, "Ski Rack", "not_available", "Vancouver International Airport", "Vancouver")
+insert into Equipment(9999999997, "Lift gate and Car-towing equipment", "available", "UBC", "Vancouver")
+insert into Equipment(0000000007, "Child Safety Seats", "rented", "Parliament", "Ottawa")
+
+-- Insertion EforV(etname, vtname)
+insert into EforV("Ski Rack", "Economy")
+insert into EforV("Child Safety Seats", "Economy")
+insert into EforV("Child Safety Seats", "SUV")
+insert into EforV("Child Safety Seats", "Standard")
+insert into EforV("Lift gate and Car-towing equipment", "Truck")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
