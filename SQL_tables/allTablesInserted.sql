8cd43964451173c40da7fa6296ebad80e498c68d
AmandaYang <yuanyuan.yang@alumni.ubc.ca>
2019-11-07 18:01:35 -0800

    [x] removed equipment, equipmentType, Club Customer, Ccard, Reserve_Includes, Rent_Includes, EforV. [x] changed primary key for Customer to dlicense [x] chanegd primary key for Vehicle to vlicense [x] changed status for vehicle to be rented, avail, or maintenance [x] Made confNO in Reservation autogenerating from 1 to 10000000; [x] fixed foreignkeys for all tables

--- SQL_tables/allTablesInserted.sql
+++ SQL_tables/allTablesInserted.sql
@@ -1,31 +1,19 @@
 
 --
 --  Database Table Creation
---
---  First drop any existing tables. Any errors are ignored.
---
+
 drop table Branch cascade constraints;
 drop table VehicleType cascade constraints;
 drop table Vehicle cascade constraints;
-drop table EquipType cascade constraints;
-drop table Equipment cascade constraints;
-drop table EforV cascade constraints;
-
-
+drop SEQUENCE res_sequence;
+drop trigger res_trigger;
 DROP TABLE Customer CASCADE CONSTRAINTS;
-DROP TABLE ClubMember CASCADE CONSTRAINTS;
 DROP TABLE Reservation CASCADE CONSTRAINTS;
-DROP TABLE Reserve_Includes CASCADE CONSTRAINTS;
 DROP TABLE Rent CASCADE CONSTRAINTS;
-DROP TABLE CCard CASCADE CONSTRAINTS;
-DROP TABLE Rent_Includes CASCADE CONSTRAINTS;
 DROP TABLE ReturnV CASCADE CONSTRAINTS;
 DROP TABLE TimePeriod CASCADE CONSTRAINTS;
 DROP TABLE Account CASCADE CONSTRAINTS;
 
---
--- Now, add each table.
---
 -- Branch (location, city) 
 create table Branch(
     location varchar(200),
@@ -43,53 +31,24 @@ create table VehicleType(
     dirate decimal check (dirate between 0 and 1000000),
     hirate decimal check (hirate between 0 and 1000000),
     krate decimal check (krate between 0 and 1000000));
--- Vehicle (vid, vlicense, make, model, year, color, odometer, status, vtname, location, city)  
--- primary key vid
+
+-- Vehicle (vlicense, make, model, year, color, odometer, status, vtname, location, city)  
+-- primary key vlicense
 -- foreignkey vtname refers to VehicleType
 -- foreignkey location, city refers to Branch
 create table Vehicle(
-    vid varchar(10) check (vid between 0 and 9999999999) primary key,
-    vlicense varchar(20),
+    vlicense varchar(8) primary key,
     make varchar(20),
     model varchar(20),
-    year int check (year between 1960 and 2020), -- year type is not supported??
+    year int check (year between 1960 and 2020),
     color varchar(20),
     odometer decimal check (odometer between 0 and 1000000),
-    status varchar(8) check (status = 'for_rent' OR status = 'for_sale'),
+    status varchar(20) check (status = 'available' OR status = 'maintenance' OR status ='rented'),
     vtname varchar(50),
     foreign key(vtname) references VehicleType,
     location varchar(200),
     city varchar(50),
     foreign key (location,city) references Branch(location,city));
-
--- EquipType(etname, drate, hrate) 
--- primary key etname
-create table EquipType(
-    etname varchar(100) primary key,
-    drate decimal check (drate between 0 and 1000000), 
-    hrate decimal check (hrate between 0 and 1000000));
-
--- Equipment (eid, etname, status, location, city) 
--- primary key eid
--- foreign key etname, location, city
--- constraints: status can take the values ( available, rented, not_available)
-create table Equipment (
-    eid varchar(10) check (eid between 0 and 9999999999) primary key, 
-    etname varchar(100),
-    foreign key(etname) references EquipType(etname), 
-    status varchar(15) check ((status = 'available') OR (status = 'not_available') OR (status = 'rented')) , 
-    location varchar(200),
-    city varchar(50),
-    foreign key (location,city) references Branch(location,city));
-
--- EforV(etname, vtname)
-create table EforV(
-    etname varchar(100),
-    vtname varchar(50),
-    primary key(etname,vtname),
-    foreign key(etname) references EquipType, 
-    foreign key(vtname) references VehicleType);
-
 
 
 CREATE TABLE TimePeriod(
@@ -101,79 +60,57 @@ CREATE TABLE TimePeriod(
 );
 
 
-
-CREATE TABLE CCard(
-    cardNo varchar(16) check (cardNo between 0000000000000000 and 9999999999999999),
-    ExpDate char(20) , -- TODO: CHECK (ExpDate >= SYSDATE)
-    cardName CHAR(40),
-    PRIMARY KEY(cardNo, ExpDate)
-);
-
 CREATE TABLE Customer(
-    cellphone varchar(10) check (cellphone between 0 and 9999999999) primary key,
+    cellphone varchar(10) check (cellphone between 0 and 9999999999),
     cname CHAR(40),
     caddress VARCHAR(50),
-    dlicense VARCHAR(20)
+    dlicense VARCHAR(20) primary key
 );
 
-CREATE TABLE ClubMember(
-    cellphone varchar(10) primary key,
-    points INTEGER check (points BETWEEN 0 and 5000),
-    fees NUMERIC check(fees BETWEEN 0  and 1000),
-    FOREIGN KEY (cellphone) REFERENCES Customer
-);
 
 CREATE TABLE Reservation(
-    confNo INTEGER CHECK (confNo BETWEEN 0 and POWER(10,20) - 1) PRIMARY KEY,
+    confNo INTEGER CHECK (confNo BETWEEN 0 and 1000000000000000) PRIMARY KEY, -- PRIMARY KEY,
     vtname varchar(50),
-    cellphone varchar(10),
+    dlicense VARCHAR(20),
     fromDate char(20),
     fromTime char(20),
     toDate char(20),
     toTime char(20),
     FOREIGN KEY (vtname) REFERENCES VehicleType,
-    FOREIGN KEY (cellphone) REFERENCES Customer,
+    FOREIGN KEY (dlicense) REFERENCES Customer,
     FOREIGN KEY (fromDate, fromTime, toDate, toTime) REFERENCES TimePeriod
 );
 
--- 
+CREATE SEQUENCE res_sequence
+  START WITH 1
+  INCREMENT BY 1
+  CACHE 1000000000000000;
 
-CREATE TABLE Reserve_Includes(
-    confNo INT,
-    etname varchar(100),
-    PRIMARY KEY (confNo, etname),
-    FOREIGN KEY (confNo) references Reservation,
-    FOREIGN KEY (etname) references EquipType
-);
+CREATE OR REPLACE TRIGGER res_trigger
+  BEFORE INSERT ON Reservation
+  FOR EACH ROW
+BEGIN
+  SELECT res_sequence.nextval
+    INTO :new.confNo
+    FROM dual;
+END;
+/
 
-
---
 
 CREATE TABLE Rent(
     rid VARCHAR(10) check (rid between 0 and 9999999999) PRIMARY KEY,
-    vid VARCHAR(15),
-    cellphone varchar(10),
+    vlicense varchar(8),
+    dlicense VARCHAR(20),
     fromDate char(20),
     fromTime char(20),
     toDate char(20),
     toTime char(20),
     odometer DECIMAL CHECK (odometer between 0 and 1000000),
-    cardNo varchar(16),
-    ExpDate char(20),
     confNo INT,
-    FOREIGN KEY (vid) REFERENCES Vehicle,
-    FOREIGN KEY (cellphone) REFERENCES Customer,
+    FOREIGN KEY (vlicense) REFERENCES Vehicle,
+    FOREIGN KEY (dlicense) REFERENCES Customer,
     FOREIGN KEY (fromDate, fromTime, toDate, toTime) REFERENCES TimePeriod,
-    FOREIGN KEY (cardNo, ExpDate) REFERENCES CCard,
     FOREIGN KEY (confNo) REFERENCES Reservation
-);
-
-CREATE TABLE Rent_Includes(
-    rid VARCHAR(10),
-    eid VARCHAR(10),
-    PRIMARY KEY (rid, eid),
-    FOREIGN KEY (rid) REFERENCES Rent,
-    FOREIGN KEY (eid) REFERENCES Equipment
 );
 
 CREATE TABLE ReturnV(
@@ -184,6 +121,7 @@ CREATE TABLE ReturnV(
     fulLTank varchar(3) check (fulLTank = 'yes' OR fulLTank = 'no'),
     FOREIGN KEY (rid) REFERENCES Rent
 );
+
 
 CREATE TABLE Account(
     username VARCHAR(15) CHECK (LENGTH(username) >= 6),
@@ -207,75 +145,39 @@ insert into VehicleType VALUES('Fullsize', 'Automatic Transmission, 4 Doors, A/C
 insert into VehicleType VALUES('SUV', 'Automatic Transmission, 4 Doors, A/C, Leather Seats', 1000.78, 300.90, 90.87, 9, 7.99, 4.23, 100);
 insert into VehicleType VALUES('Truck', 'Stick, 2 Doors, 1t Storage, Winter Tires', 1000.78, 300.90, 90.87, 9, 7.99, 4.23, 100);
 
--- Insertion Vehicle (vid, vlicense, make, model, year, color, odometer, status, vtname, location, city)    
-insert into Vehicle VALUES(0000000001, 25441163, 'BMW', 'X5', 2017, 'salmon', 9800, 'for_rent', 'SUV', 'UBC', 'Vancouver');
-insert into Vehicle VALUES(9873124183, 25441162, 'Hyundai', 'Y998', 2009, 'gray', 19823, 'for_rent', 'Economy', 'Parliament', 'Ottawa');
-insert into Vehicle VALUES(0000000002, 25441161, 'BMW', 'X5', 2017, 'salmon', 9800, 'for_sale', 'SUV', 'UBC', 'Vancouver');
+-- Insertion Vehicle ( vlicense, make, model, year, color, odometer, status, vtname, location, city)    
+insert into Vehicle VALUES(2541163, 'BMW', 'X5', 2017, 'salmon', 9800, 'available', 'SUV', 'UBC', 'Vancouver');
+insert into Vehicle VALUES(2541162, 'Hyundai', 'Y998', 2009, 'gray', 19823, 'available', 'Economy', 'Parliament', 'Ottawa');
+insert into Vehicle VALUES(2541161, 'BMW', 'X5', 2017, 'salmon', 9800, 'maintenance', 'SUV', 'UBC', 'Vancouver');
+insert into Vehicle VALUES(2541160, 'BMW', 'X5', 2016, 'red', 9800, 'rented', 'Economy', 'Vancouver International Airport', 'Vancouver');
 
--- Insert EquipType(etname, drate, hrate) 
-insert into EquipType VALUES('Ski Rack', 23, 5.99);
-insert into EquipType VALUES('Child Safety Seats', 39, 8.99);
-insert into EquipType VALUES('Lift gate and Car-towing equipment', 68.99, 10.99);
-
--- Insertion Equipment (eid, etname, status, location, city) 
-insert into Equipment VALUES(9999999999, 'Ski Rack', 'available', 'Vancouver International Airport', 'Vancouver');
-insert into Equipment VALUES(9999999998, 'Ski Rack', 'not_available', 'Vancouver International Airport', 'Vancouver');
-insert into Equipment VALUES(9999999997, 'Lift gate and Car-towing equipment', 'available', 'UBC', 'Vancouver');
-insert into Equipment VALUES(0000000007, 'Child Safety Seats', 'rented', 'Parliament', 'Ottawa');
-
--- Insertion EforV(etname, vtname)
-insert into EforV VALUES('Ski Rack', 'Economy');
-insert into EforV VALUES('Child Safety Seats', 'Economy');
-insert into EforV VALUES('Child Safety Seats', 'SUV');
-insert into EforV VALUES('Child Safety Seats', 'Standard');
-insert into EforV VALUES('Lift gate and Car-towing equipment', 'Truck');
-
--- tuples
--- customer
 
 insert into Customer values (0000000000, 'c1', '101 1st ave', 'dl101');
 INSERT into Customer values (1111111111, 'c2', 'fjioesjfoiejfoiwjf', 'dl204r34');
-
-
--- Clubmember
-insert into Clubmember VALUES (0000000000, 450, 200);
-insert into Clubmember VALUES (1111111111, 90, 3);
 
 
 -- TimePeriod 
 INSERT INTO TimePeriod VALUES (TO_DATE('2019-JAN-01', 'YYYY-MM-DD'),TO_DATE('00:00:00','HH24:MI:SS'),TO_DATE('2019-JAN-02', 'YYYY-MM-DD'),TO_DATE('00:00:10','HH24:MI:SS'));
 INSERT INTO TimePeriod VALUES (TO_DATE('2018-JAN-01', 'YYYY-MM-DD'),TO_DATE('18:09:00','HH24:MI:SS'),TO_DATE('2019-JAN-03', 'YYYY-MM-DD'),TO_DATE('23:59:10','HH24:MI:SS'));
 
-
--- Reservation
-insert into Reservation VALUES (101, 'Economy', 0000000000, TO_DATE('2019-JAN-01', 'YYYY-MM-DD'),TO_DATE('00:00:00','HH24:MI:SS'),TO_DATE('2019-JAN-02', 'YYYY-MM-DD'),TO_DATE('00:00:10','HH24:MI:SS'));
-insert into Reservation VALUES (102, 'SUV', 1111111111, TO_DATE('2018-JAN-01', 'YYYY-MM-DD'),TO_DATE('18:09:00','HH24:MI:SS'),TO_DATE('2019-JAN-03', 'YYYY-MM-DD'),TO_DATE('23:59:10','HH24:MI:SS'));
-
-
--- Reserve_includes
-INSERT into Reserve_Includes VALUES (101, 'Ski Rack');
-INSERT into Reserve_Includes VALUES (102, 'Child Safety Seats');
-
---CCard
-INSERT INTO CCard VALUES (1000200030004000, TO_DATE('2020-JAN-01', 'YYYY-MM-DD'), 'aaa');
-INSERT INTO CCard VALUES (0, TO_DATE('2020-JAN-01', 'YYYY-MM-DD'), 'aaa');
-INSERT INTO CCard VALUES (1, TO_DATE('2020-JAN-01', 'YYYY-MM-DD'), 'aaa');
-INSERT INTO CCard VALUES (2000200030004000, TO_DATE('2020-FEB-01','YYYY-MM-DD'), 'bbb');
-
-
+INSERT INTO Reservation(vtname, dlicense, fromDate, fromTime, toDate, toTime)
+  VALUES( 'Economy', 'dl101', TO_DATE('2019-JAN-01', 'YYYY-MM-DD'),TO_DATE('00:00:00','HH24:MI:SS'),TO_DATE('2019-JAN-02', 'YYYY-MM-DD'),TO_DATE('00:00:10','HH24:MI:SS'));
+INSERT INTO Reservation( vtname, dlicense, fromDate, fromTime, toDate, toTime)
+  VALUES( 'SUV', 'dl204r34', TO_DATE('2018-JAN-01', 'YYYY-MM-DD'),TO_DATE('18:09:00','HH24:MI:SS'),TO_DATE('2019-JAN-03', 'YYYY-MM-DD'),TO_DATE('23:59:10','HH24:MI:SS'));
 
 -- Rent
-INSERT INTO Rent VALUES ('101',0000000001, 0000000000,TO_DATE('2019-JAN-01', 'YYYY-MM-DD'),TO_DATE('00:00:00','HH24:MI:SS'),TO_DATE('2019-JAN-02', 'YYYY-MM-DD'),TO_DATE('00:00:10','HH24:MI:SS'), 1.1, 1000200030004000, TO_DATE('2020-JAN-01','YYYY-MM-DD'), 101);
-INSERT INTO Rent VALUES ('102',9873124183, 1111111111,TO_DATE('2018-JAN-01', 'YYYY-MM-DD'),TO_DATE('18:09:00','HH24:MI:SS'),TO_DATE('2019-JAN-03', 'YYYY-MM-DD'),TO_DATE('23:59:10','HH24:MI:SS'), 2.2, 2000200030004000, TO_DATE('2020-FEB-01','YYYY-MM-DD'), 102);
-
-
+INSERT INTO Rent (rid, vlicense, dlicense, fromDate, fromTime, toDate, toTime, odometer)
+VALUES ('1182', 2541160, 'dl101',TO_DATE('2019-JAN-01', 'YYYY-MM-DD'),TO_DATE('00:00:00','HH24:MI:SS'),TO_DATE('2019-JAN-02', 'YYYY-MM-DD'),TO_DATE('00:00:10','HH24:MI:SS'), 1.1);
+-- INSERT INTO Rent VALUES ('7762',2541163, 'dl204r34',TO_DATE('2018-JAN-01', 'YYYY-MM-DD'),TO_DATE('18:09:00','HH24:MI:SS'),TO_DATE('2019-JAN-03', 'YYYY-MM-DD'),TO_DATE('23:59:10','HH24:MI:SS'), 2.2);
+INSERT INTO Rent (rid, vlicense, dlicense, fromDate, fromTime, toDate, toTime, odometer)
+VALUES ('7762',2541163, 'dl204r34',TO_DATE('2018-JAN-01', 'YYYY-MM-DD'),TO_DATE('18:09:00','HH24:MI:SS'),TO_DATE('2019-JAN-03', 'YYYY-MM-DD'),TO_DATE('23:59:10','HH24:MI:SS'), 2.2);
 --Rent_Includes
-INSERT INTO Rent_Includes VALUES ('101',9999999999);
-INSERT INTO Rent_Includes VALUES ('102',9999999998);
+-- INSERT INTO Rent_Includes VALUES ('101',9999999999);
+-- INSERT INTO Rent_Includes VALUES ('102',9999999998);
 
 -- ReturnV
-INSERT INTO ReturnV VALUES ('101',TO_DATE('2020-JAN-01', 'YYYY-MM-DD'),TO_DATE('00:00:00', 'HH24:MI:SS'),12,'yes');
-INSERT INTO ReturnV VALUES ('102',TO_DATE('2020-JAN-02', 'YYYY-MM-DD'),TO_DATE('00:00:01', 'HH24:MI:SS'),13,'no');
+INSERT INTO ReturnV VALUES ('1182',TO_DATE('2020-JAN-01', 'YYYY-MM-DD'),TO_DATE('00:00:00', 'HH24:MI:SS'),12,'yes');
+INSERT INTO ReturnV VALUES ('7762',TO_DATE('2020-JAN-02', 'YYYY-MM-DD'),TO_DATE('00:00:01', 'HH24:MI:SS'),13,'no');
 
 -- Account
 INSERT INTO Account VALUES ('acc111', '000000000a');
